7b62c45686b2a82b022f380c677f2d20731d3786
RookieHong <120948741@qq.com>
2018-09-05 21:05:03 +0800

    增加对v5fox的支持
    增加了对v5fox的支持，微调结构以适应不同网站

--- CSGO饰品货比三家脚本.js
+++ CSGO饰品货比三家脚本.js
@@ -1,13 +1,14 @@
 ﻿// ==UserScript==
 // @name         CSGO饰品价格对比脚本
 // @connect      *
-// @version      1.5
+// @version      1.6
 // @description  将各个CSGO饰品交易平台的价格放在一起显示，省去打开多个网页的繁琐操作！
 // @author       RookieHong
 // @grant        GM_xmlhttpRequest
 // @match        https://www.c5game.com/csgo/default/*
 // @match        https://www.igxe.cn/csgo/*
 // @match        https://buff.163.com/market/?game=csgo*
+// @match        https://www.v5fox.com/csgo*
 // @require      https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js
 // ==/UserScript==
 
@@ -67,7 +68,8 @@ $(document).ready(function () {
                         .c5li{margin: 0px!important;white-space: nowrap; font-size: 12px;}\
                         .igli{padding:4px; font-size: 12px; white-space: nowrap;}\
                         .buffli{ width:auto!important; height: auto!important; float:none!important; margin: 0px!important; padding:4px!important; font-size: 12px; white-space: nowrap; border: inherit!important; border-radius: 0!important; background: #959595!important; }\
-                        .buffli a{background: #959595!important; text-align: left!important;}';
+                        .buffli a{background: #959595!important; text-align: left!important;}\
+                        .v5li {padding: 4px; font-size: 12px; white-space: nowrap;}';
     myScriptStyle = myScriptStyle + '/* 容器 <div> - 需要定位下拉内容 */\
     .dropdown {\
         position: relative;\
@@ -114,7 +116,28 @@ $(document).ready(function () {
     }\
     \
     /* 鼠标移上去后修改下拉菜单链接颜色 */\
-    .buff-dropdown-content a:hover {background-color: #f2efef!important;}';
+    .buff-dropdown-content a:hover {background-color: #f2efef!important;}\
+    \
+    /* 下拉内容 (默认隐藏) */\
+    .v5-dropdown-content {\
+        display: none;\
+        position: absolute;\
+        background-color: #1c2734!important;\
+        min-width: 160px;\
+        box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);\
+        z-index: 9999;\
+    }\
+\
+    /* 下拉菜单的链接 */\
+    .v5-dropdown-content a {\
+        color: white;\
+        padding: 12px 16px;\
+        text-decoration: none;\
+        display: block;\
+    }\
+    \
+    /* 鼠标移上去后修改下拉菜单链接颜色 */\
+    .v5-dropdown-content a:hover {background-color: #313d4d!important;}';
     addStyle(myScriptStyle);
 
     if (location.href.indexOf('c5game.com') > 0) {
@@ -125,6 +148,9 @@ $(document).ready(function () {
     }
     else if (location.href.indexOf('buff.163.com') > 0) {
         buff();
+    }
+    else if (location.href.indexOf('v5fox.com') > 0) {
+        v5fox();
     }
 });
 
@@ -147,7 +173,7 @@ function addC5(c5URL, li, itemName) {
                     return;
                 }
             }
-            if (!hasNextPage) $(li).html('<span class="myTitle">C5：</span><span style="color: #FF0000">查找不到数据！</span>');  //若没有下一页则可以判断没有该物品的数据
+            if (!hasNextPage) $(li).html('<a href="javascript:return false;" style="padding: 0px"><span class="myTitle">C5：</span><span style="color: #FF0000">查找不到数据！</span></a>');  //若没有下一页则可以判断没有该物品的数据
             else {
                 var cur_page = $(body).find('.pagination').find('.active').find('a').text();
                 var next_page = cur_page + 1;
@@ -177,7 +203,7 @@ function addIGXE(igxeURL, li, itemName) {
                     return;
                 }
             }
-            if (!hasNextPage) $(li).html('<span class="myTitle">IGXE：</span><span style="color: #FF0000">查找不到数据！</span>');  //若没有下一页则可以判断没有该物品的数据
+            if (!hasNextPage) $(li).html('<a href="javascript:return false;" style="padding: 0px"><span class="myTitle">IGXE：</span><span style="color: #FF0000">查找不到数据！</span></a>');  //若没有下一页则可以判断没有该物品的数据
             else {  //走igxe网站的流程到下一页查询
                 var page_no = $(body).find('#page-content .next').attr("page_no");  //获取下一页的页号
                 var url_param = $(body).find('#params_form').serializeObject(); //params_form是网站上的一个隐藏元素，存放各种表单信息
@@ -210,7 +236,7 @@ function addBUFF(buffURL, li, itemName) {
             }
             var total_pages = data.total_page;
             var cur_page = data.page_num;
-            if (cur_page >= total_pages) $(li).html('<span class="myTitle">BUFF：</span><span style="color: #FF0000">查找不到数据！</span>');
+            if (cur_page >= total_pages) $(li).html('<a href="javascript:return false;" style="padding: 0px"><span class="myTitle">BUFF：</span><span style="color: #FF0000">查找不到数据！</span></a>');
             else {
                 var next_page = cur_page + 1;
                 var url = 'https://buff.163.com/api/market/goods?game=csgo&page_num=' + next_page + '&search=' + itemName.trim() + '&_=' + (new Date()).valueOf().toString();
@@ -239,7 +265,7 @@ function addV5(v5URL, li, itemName) {
                     return;
                 }
             }
-            if (!hasNextPage) $(li).html('<span class="myTitle">V5FOX：</span><span style="color: #FF0000">查找不到数据！</span>');
+            if (!hasNextPage) $(li).html('<a href="javascript:return false;" style="padding: 0px"><span class="myTitle">V5FOX：</span><span style="color: #FF0000">查找不到数据！</span></a>');
             else {
                 var cur_page = $('.laypage_curr').text();
                 var next_page = cur_page + 1;
@@ -377,6 +403,51 @@ function buff() {
     $('#j_market_card').on('mouseleave', '#j_list_card li:not([class])', function () {
         if ($(this).find('div.buff-dropdown-content').length > 0) {
             $(this).find('div.buff-dropdown-content').css('display', 'none');
+            return;
+        }
+        $(this).attr('mouseover', 'false');
+    });
+}
+
+function v5fox() {
+    $('.list-box').on('mouseenter', 'a.list-item', function () {
+        if ($(this).find('div.v5-dropdown-content').length > 0) {
+            $(this).find('div.v5-dropdown-content').css('display', 'block');
+            return;
+        }
+
+        $(this).css({
+            display: 'inline - block'
+        });
+        $(this).attr('mouseover', 'true');   //当前鼠标在该物品上
+
+        var list = $('<div class="v5-dropdown-content"></div>');
+        var itemName = $(this).attr('title');
+
+        var c5URL = 'https://www.c5game.com/csgo/default/result.html?k=' + itemName + '&page=1';
+        var c5Li = $('<li class="v5li"><a href="javascript:return false;" style="padding: 0px"><span class="myTitle">C5：</span><span>载入中...</a></li>');
+        $(list).append(c5Li);
+        addC5(c5URL, c5Li, itemName);  //获取C5上的数据
+
+        var buffURL = 'https://buff.163.com/api/market/goods?game=csgo&page_num=1&search=' + itemName.trim() + '&_=' + (new Date()).valueOf().toString();
+        var buffLi = $('<li class="v5li"><a href="javascript:return false;" style="padding: 0px"><span class="myTitle">BUFF：</span><span>载入中...</a></li>');
+        $(list).append(buffLi);
+        addBUFF(buffURL, buffLi, itemName);  //获取BUFF上的数据
+
+        var igxeURL = 'https://www.igxe.cn/csgo/730?keyword=' + itemName;
+        var igxeLi = $('<li class="v5li"><a href="javascript:return false;" style="padding: 0px"><span class="myTitle">IGXE：</span><span>载入中...</a></li>');
+        $(list).append(igxeLi);
+        addIGXE(igxeURL, igxeLi, itemName);  //获取igxe上的数据
+
+        if ($(this).attr('mouseover') == 'true') {   //若鼠标还在该物品上就不隐藏刚创建的列表
+            $(list).css('display', 'block');
+        }
+        $(this).append(list);
+    });
+
+    $('.list-box').on('mouseleave', 'a.list-item', function () {
+        if ($(this).find('div.v5-dropdown-content').length > 0) {
+            $(this).find('div.v5-dropdown-content').css('display', 'none');
             return;
         }
         $(this).attr('mouseover', 'false');
